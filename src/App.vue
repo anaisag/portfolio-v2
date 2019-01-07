89913806b0ba7c66b6d76cdad7c07083646bdfea
Anaïs <anais.appereg@gmail.com>
2019-01-07 19:02:04 +0100

    ⚙️ Setting up the project & beginning footer

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,31 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	min-height: 100vh;
 }
+
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	padding: 30px;
+	&.router-link-exact-active {
+		color: #42b983;
+	}
 }
 </style>
+
+<script>
+// @ is an alias to /src
+import Footer from '@/components/Footer.vue';
+
+export default {
+	name: 'about',
+	components: {
+		Footer,
+	},
+};
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="footer">
+    <div class="contacts">
+      <div class="contacts--contacts">
+        <a href="mailto:anais.appereg@gmail.com"><p>anais.appereg@gmail.com</p></a>
+        <a href="tel:0615842193"><p>06.15.84.21.93</p></a>
+      </div>
+      <div class="contacts--socials">
+        <a href="https://www.behance.net/thermopoli6590" target="_blank">
+          <img src="@/assets/behance.svg" alt="">
+        </a>
+        <a href="https://www.linkedin.com/in/anais-appere-guillemot/" target="_blank">
+          <img src="@/assets/linkedin.svg" alt="">
+        </a>
+        <a href="https://twitter.com/anaisappere" target="_blank">
+          <img src="@/assets/twitter.svg" alt="">
+        </a>
+      </div>
+    </div>
+    <div class="mentions">
+      <p> {{new Date().getFullYear()}} // designed and integrated by Anaïs AG</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Footer',
+};
+</script>
+
+<style scoped lang="scss">
+.footer {
+	position: absolute;
+	bottom: 0;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	align-items: center;
+	background-image: url('../assets/footer.svg');
+	background-position: bottom;
+	background-repeat: no-repeat;
+	color: white;
+	padding: 0 8% 0 8%;
+	min-height: 149px;
+	width: 100%;
+
+	.contacts {
+		font-size: 15px;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		&--contacts p {
+			text-decoration-style: none;
+			color: white;
+			margin: 3%;
+		}
+		&--socials {
+			min-width: 15%;
+			display: flex;
+			justify-content: space-between;
+			img {
+				height: 20px;
+			}
+		}
+	}
+	.mentions {
+		font-size: 11px;
+		p {
+			margin-top: 0;
+		}
+	}
+}
+</style>
